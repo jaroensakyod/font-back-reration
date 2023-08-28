10ce7871ee882908af0ce1d0d74ee7d7bdb8d55f
jaroensakyod <jaroensak.yod@gmail.com>
2023-08-28 21:41:03 +0700

    config env
    Signed-off-by: jaroensakyod <jaroensak.yod@gmail.com>

--- database/firebase.bd.js
+++ database/firebase.bd.js
@@ -1,22 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import dotenv from 'dotenv';
+import * as firebase from 'firebase/app';
+import 'firebase/database';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+dotenv.config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAF8tDeZXO_qBZ_OR1zMB6e9hCEfvDe85c",
-  authDomain: "userapi-7668a.firebaseapp.com",
-  projectId: "userapi-7668a",
-  storageBucket: "userapi-7668a.appspot.com",
-  messagingSenderId: "594689124427",
-  appId: "1:594689124427:web:076f2c5fff26795d71c5ce"
-};
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+        };
+console.log(firebaseConfig)
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
 
 // Get a list of cities from your database
 const userNameAPI = async function getUser(db) {
@@ -28,3 +36,5 @@ const userNameAPI = async function getUser(db) {
 
 const userApiList = await userNameAPI(db) 
 console.log(userApiList)
+
+export default db
